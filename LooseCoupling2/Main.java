3b097b4e70a4ca763c9b024d3366ee485ac7531b
Sakshi13-05 <sakshichavan611162gmail.com>
2026-09-14 13:19:48 +0530

    Implemneted Loose Coupling

--- LooseCoupling2/Main.java
+++ LooseCoupling2/Main.java
@@ -1,10 +1,11 @@
 package BackendFoundation.LooseCoupling2;
 
 import BackendFoundation.LooseCoupling2.Notification.EmailNotification;
+import BackendFoundation.LooseCoupling2.Notification.SMSNotifiaction;;
 
 class Main {
     public static void main(String[] args) {
-        OrderService service = new OrderService(new EmailNotification());
+        OrderService service = new OrderService(new SMSNotifiaction());
         service.placeOrder();
     }
 
