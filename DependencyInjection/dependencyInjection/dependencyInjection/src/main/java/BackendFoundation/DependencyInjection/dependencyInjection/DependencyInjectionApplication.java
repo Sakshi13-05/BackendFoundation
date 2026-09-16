7267369ef01a621447692ad2e53616cd4251ba51
Sakshi13-05 <sakshichavan611162gmail.com>
2026-09-16 20:59:00 +0530

    Implemented using ConfigurableApplicationContext Interface

--- DependencyInjection/dependencyInjection/dependencyInjection/src/main/java/BackendFoundation/DependencyInjection/dependencyInjection/DependencyInjectionApplication.java
+++ DependencyInjection/dependencyInjection/dependencyInjection/src/main/java/BackendFoundation/DependencyInjection/dependencyInjection/DependencyInjectionApplication.java
@@ -2,17 +2,23 @@ package BackendFoundation.DependencyInjection.dependencyInjection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
 
 import BackendFoundation.DependencyInjection.dependencyInjection.service.OrderService;
 import BackendFoundation.DependencyInjection.dependencyInjection.service.RazorPayService;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionApplication.class, args);
-		RazorPayService razor = new RazorPayService();
-		OrderService service = new OrderService(razor);
+		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
+		// By self--ignore spring ioc
+		// RazorPayService razor = new RazorPayService();
+		// OrderService service = new OrderService(razor);
+		// service.payForOrder();
+
+		OrderService service = context.getBean(OrderService.class);
 		service.payForOrder();
 	}
 
